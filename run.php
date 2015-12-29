d61e56f72af82d27fbff9dc9b2bcb25af8e66e8e
Erik Figueiredo <falecom@webdevbr.com.br>
2015-12-29 12:48:11 -0200

    programando para uma implementacao

--- run.php
+++ run.php
@@ -4,3 +4,7 @@ include 'vendor/autoload.php';
 
 $html = new WebDevBr\Html\Html;
 
+$img = $html->img('logo.png');
+echo $html->a($img, 'https://www.webdevbr.com.br/', ['class'=>'btn btn-primary']);
+
+echo PHP_EOL;
--- src/Html.php
+++ src/Html.php
@@ -4,5 +4,26 @@ namespace WebDevBr\Html;
 
 class Html
 {
+    public function a($text, $href, array $attribute = null)
+    {
+        $attribute = $this->attribute($attribute);
+        return "<a href=\"{$href}\"{$attribute}>{$text}</a>";
+    }
 
+    public function img($src, array $attribute = null)
+    {
+        $attribute = $this->attribute($attribute);
+        return "<img src=\"{$src}\"{$attribute}>";
+    }
+
+    private function attribute($attribute)
+    {
+        if ($attribute !== null) {
+            foreach ($attribute as $k => $v) {
+                $data[] = $k.'="'.$v.'"';
+            }
+            $attribute = ' '.implode(' ', $data);
+        }
+        return $attribute;
+    }
 }
